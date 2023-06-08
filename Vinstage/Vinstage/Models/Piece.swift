63b2b25becbb55dea68b35ff487c9d501f2b8a71
Roee Kleiner <roeekleiner@gmail.com>
2023-06-08 11:22:27 +0300

    Add first models

--- Vinstage/Vinstage/Models/Piece.swift
+++ Vinstage/Vinstage/Models/Piece.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct Piece: Identifiable {
+    var id = UUID()
+    let title: String
+    let userId: UUID
+    let description: String
+    let location: Location
+    let imageUrl: String
+    let timeUploaded: Date
+}
