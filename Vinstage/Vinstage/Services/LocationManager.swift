87e72c4c028a0653a196c5d8a9912fb287ebc839
Roee Kleiner <roeekleiner@gmail.com>
2023-06-08 11:29:58 +0300

    Add location manager

--- Vinstage/Vinstage/Services/LocationManager.swift
+++ Vinstage/Vinstage/Services/LocationManager.swift
@@ -1,8 +1,73 @@
-//
-//  LocationManager.swift
-//  Vinstage
-//
-//  Created by Roee Kleiner on 08/06/2023.
-//
-
+import Combine
+import CoreLocation
 import Foundation
+
+class LocationDataManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var locationManager = CLLocationManager()
+    @Published var authorizationStatus: CLAuthorizationStatus?
+    @Published var lastLocation: CLLocation?
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.showsBackgroundLocationIndicator = true
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedAlways:
+            authorizationStatus = .authorizedWhenInUse
+            locationManager.requestLocation()
+            break
+        case .authorizedWhenInUse:  // Location services are available.
+            // Insert code here of what should happen when Location services are authorized
+
+            authorizationStatus = .authorizedWhenInUse
+            locationManager.requestLocation()
+            break
+
+        case .restricted:  // Location services currently unavailable.
+            // Insert code here of what should happen when Location services are NOT authorized
+            authorizationStatus = .restricted
+            break
+
+        case .denied:  // Location services currently unavailable.
+            // Insert code here of what should happen when Location services are NOT authorized
+            authorizationStatus = .denied
+            break
+
+        case .notDetermined:  // Authorization not determined yet.
+            authorizationStatus = .notDetermined
+            break
+
+        default:
+            break
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        lastLocation = location
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        guard let clError = error as? CLError else {
+            return
+        }
+        print("error: \(error.localizedDescription)")
+    }
+
+    func startLocationUpdates() {
+        locationManager.startUpdatingLocation()
+    }
+
+    func stopLocationUpdates() {
+        locationManager.stopUpdatingLocation()
+    }
+
+    func requestAlwaysAuth() {
+        locationManager.requestAlwaysAuthorization()
+    }
+
+}
