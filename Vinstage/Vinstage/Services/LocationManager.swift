f8bad18605d89f9fba908679492679382a39aab0
Roee Kleiner <roeekleiner@gmail.com>
2023-06-09 06:06:54 +0300

    Fix services to work correctly with the views

--- Vinstage/Vinstage/Services/LocationManager.swift
+++ Vinstage/Vinstage/Services/LocationManager.swift
@@ -2,23 +2,27 @@ import Combine
 import CoreLocation
 import Foundation
 
-class LocationDataManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     var locationManager = CLLocationManager()
     @Published var authorizationStatus: CLAuthorizationStatus?
-    @Published var lastLocation: CLLocation?
+    @Published var lastLocation: Location?
     
     override init() {
         super.init()
         locationManager.delegate = self
-        locationManager.allowsBackgroundLocationUpdates = true
-        locationManager.showsBackgroundLocationIndicator = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.pausesLocationUpdatesAutomatically = true
+        if locationManager.authorizationStatus == .authorizedWhenInUse{
+//            locationManager.startUpdatingLocation()
+
+        }
     }
 
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         switch manager.authorizationStatus {
         case .authorizedAlways:
             authorizationStatus = .authorizedWhenInUse
-            locationManager.requestLocation()
+//            locationManager.startUpdatingLocation()
             break
         case .authorizedWhenInUse:  // Location services are available.
             // Insert code here of what should happen when Location services are authorized
@@ -48,11 +52,12 @@ class LocationDataManager: NSObject, ObservableObject, CLLocationManagerDelegate
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
-        lastLocation = location
+        lastLocation = Location(clLocation: location)
+//        stopLocationUpdates()
     }
 
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        guard let clError = error as? CLError else {
+        guard error is CLError else {
             return
         }
         print("error: \(error.localizedDescription)")
@@ -66,8 +71,8 @@ class LocationDataManager: NSObject, ObservableObject, CLLocationManagerDelegate
         locationManager.stopUpdatingLocation()
     }
 
-    func requestAlwaysAuth() {
-        locationManager.requestAlwaysAuthorization()
+    func requestAuth() {
+        locationManager.requestWhenInUseAuthorization()
     }
 
 }
--- Vinstage/Vinstage/Services/PieceService.swift
+++ Vinstage/Vinstage/Services/PieceService.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+
+class PieceService {
+
+    let apiURL = URL(string: "http://192.168.137.1:5000/api")!
+    let jsonDecoder: JSONDecoder
+    let jsonEncoder: JSONEncoder
+    init() {
+        jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        jsonDecoder.dateDecodingStrategy = .secondsSince1970
+        jsonEncoder = JSONEncoder()
+        jsonEncoder.outputFormatting = .prettyPrinted
+        jsonEncoder.keyEncodingStrategy = .convertToSnakeCase
+        jsonEncoder.dateEncodingStrategy = .secondsSince1970
+    }
+
+    func uploadNewPiece(_ piece: Piece) async {
+        var request = URLRequest(url: apiURL)
+        request.httpMethod = "POST"
+        do {
+            let data = try jsonEncoder.encode(piece)
+            print(String(data: data, encoding: .utf8)!)
+            request.httpBody = data
+
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            print(request.httpBody!)
+            let (respData, response) = try await URLSession.shared.data(for: request)
+            if let httpResponse = response as? HTTPURLResponse {
+                print(httpResponse.statusCode)
+                print(String(data: respData, encoding: .utf8)!)
+
+            }
+        } catch {
+            print("Error encoding piece")
+            return
+        }
+    }
+
+    func getNearbyPieces(location: Location, radius: Int) async -> [Piece] {
+        //        return Piece.samplePieces
+        var url = URLComponents(url: apiURL, resolvingAgainstBaseURL: true)!
+        url.queryItems = [
+            URLQueryItem(name: "x", value: String(location.latitude)),
+            URLQueryItem(name: "y", value: String(location.longitude)),
+            URLQueryItem(name: "radius", value: String(radius)),
+        ]
+        var request = URLRequest(url: url.url!)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            if let httpResponse = response as? HTTPURLResponse {
+                print(httpResponse.statusCode)
+            }
+            print(String(data: data, encoding: .utf8)!)
+            return try jsonDecoder.decode([Piece].self, from: data)
+
+        } catch {
+            print(error)
+
+        }
+        return []
+    }
+}
